fc730c90d54ea6246a3c9c1394fb7c5a13a16ffa
mohan <sahumohan338@gmail.com>
2022-11-17 11:07:29 +0530

    [mohan]add: Example of Inheritence

---
+++ InheritenceExample/InheritenceExample/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace InheritenceExample
+{
+   public class Employee
+    {
+        public double salary = 50000;
+    }
+    public class Programmmer : Employee
+    {
+       public double bonus = 1000;
+    }
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            Programmmer programmmer = new Programmmer();
+            Console.WriteLine(programmmer.salary+" "+programmmer.bonus);
+        }
+    }
+}
