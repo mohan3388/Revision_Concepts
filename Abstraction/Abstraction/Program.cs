c95a0e6a2f028f5ad3569996a640e294cd8b9f83
mohan <sahumohan338@gmail.com>
2022-11-17 12:13:07 +0530

    [mohan]add: Example of Abstraction

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Abstraction
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+    } 
+
+    public class Ractangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Ractangle");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            Shape shap = new Ractangle();
+            shap.draw();
+            Shape shape= new Circle();
+            shape.draw();
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Polymorphism
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal eating");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Dog eating");
+        }
+    }
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            Animal animal = new Dog();
+            animal.eat();
+        }
+    }
+}
