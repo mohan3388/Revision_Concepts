bae443e390264bedc0ecd25e9227172a4bab9b7a
mohan <sahumohan338@gmail.com>
2022-11-17 12:26:27 +0530

    [mohan]add: Example of Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Encapsulation
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            student st = new student();
+            st.Id = 1;
+            st.Name = "govind";
+
+            Console.WriteLine(st.Id + " " + st.Name);
+        }
+    }
+}
