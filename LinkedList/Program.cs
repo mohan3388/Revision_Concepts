fac98e305980482b8483ebc77c943990dbc8d270
mohan <sahumohan338@gmail.com>
2022-11-18 10:13:16 +0530

    [mohan]add: Example of LinkedList

---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("a");
+            list.AddLast("b");
+            list.AddLast("c");
+            list.AddFirst("d");
+            foreach(string item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
