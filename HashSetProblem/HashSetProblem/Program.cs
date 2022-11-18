02c57f5049a70a5f0777f7ccaeda6e7bdef540a4
mohan <sahumohan338@gmail.com>
2022-11-18 15:11:49 +0530

    [mohan]add: Example of Delegate

---
+++ HashSetProblem/HashSetProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HashSetProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("hii");
+            set.Add("mks");
+
+            foreach(var a in set)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
