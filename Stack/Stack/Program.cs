29f2c7f6317860940142792010e3e49399e7f530
mohan <sahumohan338@gmail.com>
2022-11-18 10:23:09 +0530

    [mohan]add: Example of stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace StackList
+{
+    class Stacks
+    {
+        public static void Main(string[] args)
+        {
+            Stack list = new Stack();
+            list.Push(1);
+            list.Push(2);
+            list.Push("abc");
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
