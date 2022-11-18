b86e8bfc30d84bd50ef8b8e420756d8ae40f27fe
mohan <sahumohan338@gmail.com>
2022-11-18 12:18:04 +0530

    [mohan]add: Example of Hashtable

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace HashTable
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("1", "raj");
+            ht.Add("2", "mks");
+            foreach(string key in ht.Keys)
+            {
+                Console.WriteLine(key+" " + ht[key]);
+            }
+        }
+    }
+}
