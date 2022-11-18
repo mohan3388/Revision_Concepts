045e0bb78f9777de9a876e0d72fbf03361873c87
mohan <sahumohan338@gmail.com>
2022-11-18 18:37:10 +0530

    [mohan]add: Example of pattern

---
+++ RegularExpression/RegularExpression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Enter Name: ");
+                string name = Console.ReadLine();
+                string pattrn = "^[A-Z]{1,}[A-Za-z]{2,}";
+                if (Regex.IsMatch(name, pattrn))
+                {
+                    Console.WriteLine("valid name " + name);
+                }
+                else
+                {
+                    Console.WriteLine("not valid");
+                };
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
