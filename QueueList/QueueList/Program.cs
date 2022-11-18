4146d67e0e05af83c3f22e1429755765504fb42e
mohan <sahumohan338@gmail.com>
2022-11-18 12:09:59 +0530

    [mohan]add: Example of queue

---
+++ QueueList/QueueList/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace QueueList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+            queue.Dequeue();
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Stack/Stack/Program.cs
+++ Stack/Stack/Program.cs
@@ -11,6 +11,7 @@ namespace StackList
             list.Push(1);
             list.Push(2);
             list.Push("abc");
+            list.Pop();
             foreach(var item in list)
             {
                 Console.WriteLine(item);
