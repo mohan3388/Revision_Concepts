200c9f456fb3d638a96f16f5c5ec9ad3901f582f
mohan <sahumohan338@gmail.com>
2022-11-20 12:44:57 +0530

    [mohan]add: File Io

---
+++ FileIOStream/FileIOStream/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FileIOStream
+{
+    class Test
+    {
+        string path = @"G:\RIvisionConcept\Revision_Concepts\FileIOStream\FileIOStream\TextFile.txt";
+        string[] lines;
+        public void Readtext()
+        {
+            lines=File.ReadAllLines(path);
+            for (int i = 0; i < lines.Length; i++) {
+                Console.WriteLine(lines[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Test test = new Test();
+            test.Readtext();
+        }
+    }
+}
