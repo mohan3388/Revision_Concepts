80c78929e93bf6881a6a6febf6fb3ab582b8729e
mohan <sahumohan338@gmail.com>
2022-11-20 12:19:27 +0530

    [mohan]add: Lmbda Expression

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LambdaExpression
+{
+    class test
+    {
+       
+        string Pattern = "^[A-Z]{1,}[a-z]{2,}$";
+        public string CheckName(string name) => Regex.IsMatch(name, Pattern) ? "Name is Valid" : "Name is not Valid";
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            test valid = new test();
+            string user = valid.CheckName(name);
+            if (user == "Name is Valid")
+            {
+                Console.WriteLine(user);
+            }
+            else
+            {
+                Console.WriteLine(user);
+            }
+
+        }
+    }
+}
